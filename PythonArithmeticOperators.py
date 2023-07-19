1e2daca6197202dabd037ba43fbf4bcf8504d9c7
Nicole Fernanda Miranda de Arruda <8853@in.cassems.com.br>
2023-07-19 13:51:16 -0400

    python exercises

---
+++ PythonArithmeticOperators.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+sum_of_two_numbers = a + b
+difference_of_two_numbers = a - b
+product_of_two_numbers = a * b
+
+print(sum_of_two_numbers)
+print(difference_of_two_numbers)
+print(product_of_two_numbers)
