ced31ea25cecdcc417b6f1da69d1843773fafc5b
Nicole Fernanda Miranda de Arruda <8853@in.cassems.com.br>
2023-07-21 14:33:47 -0400

    day 08

---
+++ Python/WriteFunction.py
@@ -0,0 +1,11 @@
+year = int(input())
+
+def is_leap(year):
+  if (year % 4 == 0) or (year % 100 == 0 and year % 400 == 0): 
+    leap = True
+  else:
+    leap = False
+  return leap
+
+result = is_leap(year)
+print(result)
